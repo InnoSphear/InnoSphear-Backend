4b6d02bcf93d7a3ce6faa60052f1e59630330246
InnoSphear <innosphear@gmail.com>
2026-02-23 15:57:44 +0530

    fix sharp render issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 connectDb().then(() => seedSuperAdmin());
 
 app.use(helmet());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
